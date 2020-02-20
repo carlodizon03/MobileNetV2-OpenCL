6c3a9fb3b2a9f022d7a9eaa9e6d83212bbb209c3
carlodizon03 <carlodizon03@gmail.com>
2020-02-20 18:55:35 +0800

    Changed file paths for cl file, images., and weight

--- MobileNet_v2_dev.cpp
+++ MobileNet_v2_dev.cpp
@@ -4,9 +4,9 @@
 int main()
 {
 	mobilenetv2 model(0, 0);
-	const char* clPath = "D:/Documents/Course Materials/Deep Learning Projects/Liscotech MobileNEtv2/MobileNetV2_IDO/MobileNetv2 -OpenCl Implementation/MobileNetv-OpenCL/mobilenetv2.cl";
-	const char* weightPath = "D:/Documents/Course Materials/Deep Learning Projects/Liscotech MobileNEtv2/MobileNetV2_IDO/MobileNetv2 -OpenCl Implementation/MobileNetv-OpenCL/weight.bin";
-	const char* imgPath = "D:/Documents/Course Materials/Deep Learning Projects/Liscotech MobileNEtv2/MobileNetV2_IDO/MobileNetv2 -OpenCl Implementation/MobileNetv-OpenCL/test_img/test1.bmp";
+	const char* clPath = "mobilenetv2.cl";
+	const char* weightPath = "weight.bin";
+	const char* imgPath = "test_img/test1.bmp";
 	model.clInitialize(clPath, weightPath);
 	model.clImageLoader(imgPath);
 	//model.test();
